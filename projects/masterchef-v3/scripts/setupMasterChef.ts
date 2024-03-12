50009bc06016bba5f992881492e32677aff696ea
CamilleBzd <bcamille99@gmail.com>
2024-03-12 15:51:34 +0000

    [ADD]: setupMasterChef

--- projects/masterchef-v3/scripts/setupMasterChef.ts
+++ projects/masterchef-v3/scripts/setupMasterChef.ts
@@ -3,6 +3,8 @@ import { DeploymentsGlobal } from "../../../deployments/deploymentsGlobalType";
 
 type Deployments = {
   MasterChefV3: string,
+  MasterChefV3ReceiverV2: string,
+  MasterChefV3KeeperV2: string
 };
 
 async function main() {
@@ -18,8 +20,16 @@ async function main() {
   // set lm pool + set receiver
   await (await masterChefV3.setLMPoolDeployer(deploymentsGlobal.PancakeV3LmPoolDeployer)).wait();
   // masterchef receiver v2
-  // await (await masterChefV3.setReceiver()).wait();
+  await (await masterChefV3.setReceiver(deployments.MasterChefV3ReceiverV2)).wait();
   console.log("LM pool deployer and owner set on Factory.");
+
+  // set operator with keeper as arg on receiver
+  const masterChefV3ReceiverV2 = await ethers.getContractAt("MasterChefV3ReceiverV2", deployments.MasterChefV3ReceiverV2);
+  await (await masterChefV3ReceiverV2.setOperator(deployments.MasterChefV3KeeperV2)).wait();
+
+  // set on keeper set period duration: 129600 + set registry: chainlink upkeep etc
+  const masterChefV3KeeperV2 = await ethers.getContractAt("MasterChefV3KeeperV2", deployments.MasterChefV3KeeperV2);
+  await (await masterChefV3KeeperV2.setPeriodDuration(129600)).wait();
 }
 
 main()
