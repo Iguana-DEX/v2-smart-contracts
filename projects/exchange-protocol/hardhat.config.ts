aae170145f815bb171f45a6b23e04cbbb82ae42a
styliann-eth <ns2808@protonmail.com>
2024-03-31 23:02:52 +0100

    feat: deploy and verify scripts

--- projects/exchange-protocol/hardhat.config.ts
+++ projects/exchange-protocol/hardhat.config.ts
@@ -6,10 +6,18 @@ import "hardhat-abi-exporter";
 import "hardhat-contract-sizer";
 import "solidity-coverage";
 import "dotenv/config";
+import "@nomicfoundation/hardhat-verify";
+
 
 const bscTestnet: NetworkUserConfig = {
   url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
   chainId: 97,
+  accounts: [process.env.KEY_TESTNET!],
+};
+
+const etherlinkTestnet: NetworkUserConfig = {
+  url: "https://node.ghostnet.etherlink.com",
+  chainId: 128123,
   accounts: [process.env.KEY_TESTNET!],
 };
 
@@ -19,13 +27,46 @@ const bscMainnet: NetworkUserConfig = {
   accounts: [process.env.KEY_MAINNET!],
 };
 
+const goerli: NetworkUserConfig = {
+  url: 'https://rpc.ankr.com/eth_goerli',
+  chainId: 5,
+  accounts: [process.env.KEY_GOERLI!],
+}
+
+const eth: NetworkUserConfig = {
+  url: 'https://eth.llamarpc.com',
+  chainId: 1,
+  accounts: [process.env.KEY_ETH!],
+}
+
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "etherlinkTestnet",
+        chainId: 128123,
+        urls: {
+          apiURL: "https://testnet-explorer.etherlink.com/api",
+          browserURL: "https://testnet-explorer.etherlink.com"
+        }
+      }
+    ],
+  },
   networks: {
     hardhat: {},
-    // testnet: bscTestnet,
-    // mainnet: bscMainnet,
+    ...(process.env.KEY_TESTNET && { bscTestnet }),
+    ...(process.env.KEY_TESTNET && { etherlinkTestnet }),
+    ...(process.env.KEY_MAINNET && { bscMainnet }),
+    ...(process.env.KEY_GOERLI && { goerli }),
+    ...(process.env.KEY_ETH && { eth }),
   },
+  // networks: {
+  //   hardhat: {},
+  //   // testnet: bscTestnet,
+  //   // mainnet: bscMainnet,
+  // },
   solidity: {
     compilers: [
       {
